5893439b5594fc33eec9b95b889c710109110bed
temich91 <temich-ivanoff@yandex.ru>
2025-04-09 18:34:49 +0500

    Homework 2 fix 2 (text processing by every word in keyword analyzer)

--- src/main/java/edu/phystech/hw2/analyzer/KeywordAnalyzer.java
+++ src/main/java/edu/phystech/hw2/analyzer/KeywordAnalyzer.java
@@ -14,26 +14,12 @@ public abstract class KeywordAnalyzer implements TextAnalyzer {
 
     @Override
     public Label processText(String text) {
-        int textSize = text.length();
-        String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        String[] whitespaceSplit = text.split(" ");
         for (String keyword : keywords) {
-            int i = 0;
-            int keywordSize = keyword.length();
-            while (i + keywordSize <= textSize) {
-                String substr = text.substring(i, i + keywordSize);
-                if (substr.equals(keyword)) {
-                    if (i + keywordSize - 1 == textSize) {
-                        return label;
-                    }
-                    if ((i > 0) && (LETTERS.indexOf(text.charAt(i - 1)) == -1)) {
-                        return label;
-                    }
-
-                    if ((i + keywordSize < textSize) && (LETTERS.indexOf(text.charAt(i + 1)) == -1)) {
-                        return label;
-                    }
+            for (String word : whitespaceSplit) {
+                if (word.equals(keyword)) {
+                    return label;
                 }
-                i++;
             }
         }
         return Label.OK;
